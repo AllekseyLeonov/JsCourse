5fb2c7c3d7b055215df1e4d7c1bbf67ecdffab05
AllekseyLeonov <leonovalleksey@gmail.com>
2021-04-14 13:43:02 +0300

    fix filter.js

--- Task1/filter/filter.js
+++ Task1/filter/filter.js
@@ -1,15 +1,23 @@
-function filter(arr, condition){
-  let newArr = []
-  for(let i = 0; i < arr.length; i++){
-    if(condition(arr[i])){
-      newArr.push(arr[i])
+function filter(array, condition){
+  const newArr = []
+  if(typeof condition != "function"){
+    console.log("Error:'filter' function second argument was not a function")
+  }
+  else if(condition.length !== 1){
+    console.log("Error:'filter' function second argument must have one argument")
+  }
+  else {
+    for(let i = 0; i < array.length; i++){
+      if(condition(array[i])) {
+        newArr.push(array[i])
+      }
     }
   }
   return newArr
 }
 
-let arr = [1,2,3,4]
+const arr = [1,2,3,4]
 console.log(arr)
-
-arr = filter(arr, i => i>2)
-console.log(arr)
+console.log(filter(arr, item =>  item>2))
+console.log(filter(arr, 3))
+console.log(filter(arr, (item,i) =>  item>2))
