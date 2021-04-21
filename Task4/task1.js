aca63cf3ee829da9d48241ded255541ea50afcaf
AllekseyLeonov <leonovalleksey@gmail.com>
2021-04-21 16:10:24 +0300

    Add Task4

---
+++ Task4/task1.js
@@ -0,0 +1,14 @@
+﻿const DURATION = 2000;
+
+const delay = (duration) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+
+function logHi() {
+  console.log("Hi");
+}
+
+delay(DURATION).then(logHi);
---
+++ Task4/task2.js
@@ -0,0 +1,15 @@
+﻿function makeDroids() {
+  const droids = [];
+  // var --> let
+  for (let i = 0; i < 10; i += 1) {
+    const droid = () => {
+      console.log(`D${i}`);
+    };
+    droids.push(droid);
+  }
+  return droids;
+}
+
+for (const d of makeDroids()) {
+  d();
+}
