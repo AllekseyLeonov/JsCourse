b316f6e7ab1363a4a00bbdbfce06622d3223924b
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-04 09:21:09 +0300

    Complete task 6 (first react app task)

---
+++ notes/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Note from "./components/note";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Note />
+    </div>
+  );
+}
+
+export default App;
