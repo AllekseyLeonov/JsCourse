aea211b8417bac434854fb2145cfed328bd07a1b
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-14 01:46:19 +0300

    [V1-03] add current-note-field component

---
+++ notes/src/components/current-note-field.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "50px 50px",
+    textAlign: "justify",
+  },
+});
+
+const CurrentNoteField = ({ selectedItem }) => (
+  <Grid
+    className={useStyles().root}
+    container
+    direction="column"
+    alignItems="center"
+  >
+    <Typography variant="h3">{selectedItem.title}</Typography>
+    <Typography variant="h5">{selectedItem.content}</Typography>
+  </Grid>
+);
+
+CurrentNoteField.propTypes = {
+  selectedItem: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    date: PropTypes.string,
+  }),
+};
+
+CurrentNoteField.defaultProps = {
+  selectedItem: { title: "Select note to display" },
+};
+
+export default CurrentNoteField;
