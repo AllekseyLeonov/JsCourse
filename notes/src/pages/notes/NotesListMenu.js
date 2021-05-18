f5ca6b64541bd704153bd4bae9d870a195e268c3
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-18 17:44:42 +0300

    create new component to unit all the components in left menu with the list of note

---
+++ notes/src/pages/notes/NotesListMenu.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+import Styles from "./styles";
+import SearchField from "../../components/SearchField";
+import NotesList from "./NotesList";
+
+const NotesListMenu = ({ notesArray, selectedIndex, changeSelectedIndex }) => (
+  <Grid
+    className={Styles().notesListMenu}
+    container
+    item
+    xs={3}
+    direction="column"
+  >
+    <SearchField />
+    <NotesList
+      notesArray={notesArray}
+      selectedIndex={selectedIndex}
+      setSelectedIndex={changeSelectedIndex}
+    />
+  </Grid>
+);
+
+NotesListMenu.propTypes = {
+  notesArray: PropTypes.arrayOf(PropTypes.object),
+  selectedIndex: PropTypes.number,
+  changeSelectedIndex: PropTypes.func,
+};
+
+NotesListMenu.defaultProps = {
+  notesArray: [],
+  selectedIndex: -1,
+  changeSelectedIndex() {},
+};
+
+export default NotesListMenu;
