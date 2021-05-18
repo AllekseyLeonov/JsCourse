65e91381aa6cdc2a4b0a1d39509d1aa6132bc932
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-18 17:24:57 +0300

    Create file for scc instead of using 'makestyles' in components

---
+++ notes/src/pages/notes/styles.js
@@ -0,0 +1,18 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const Styles = makeStyles({
+  root: {
+    padding: "50px 50px",
+    textAlign: "justify",
+  },
+  notesListMenu: {
+    borderRightColor: "red",
+    borderRightWidth: "3px",
+    borderRightStyle: "solid",
+    borderBottomColor: "red",
+    borderBottomWidth: "3px",
+    borderBottomStyle: "solid",
+  },
+});
+
+export default Styles;
