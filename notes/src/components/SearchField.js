3540c6552fc9b2d3d5e36d80731114cad6cf579c
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-18 17:35:49 +0300

    replace react import for not to separate material-ui imports

--- notes/src/components/SearchField.js
+++ notes/src/components/SearchField.js
@@ -1,6 +1,6 @@
+import React from "react";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
-import React from "react";
 import Input from "@material-ui/core/Input";
 
 const SearchField = () => (
