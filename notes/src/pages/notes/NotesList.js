4148b89d12f0717c511921f97abb605dcbf77904
AllekseyLeonov <leonovalleksey@gmail.com>
2021-05-19 12:54:51 +0300

    remove handleListItemClick function

--- notes/src/pages/notes/NotesList.js
+++ notes/src/pages/notes/NotesList.js
@@ -11,38 +11,33 @@ import { getPreviewString } from "../../utils/textFormatUtils";
 
 const LENGTH_OF_NOTE_CONTENT_PREVIEW = 20;
 
-const NotesList = ({ notesArray, selectedIndex, setSelectedIndex }) => {
-  const handleListItemClick = (index) => {
-    setSelectedIndex(index);
-  };
-  return (
-    <List>
-      {notesArray.map((item) => (
-        <ListItem
-          button
-          selected={selectedIndex === item.id}
-          onClick={() => handleListItemClick(item.id)}
-        >
-          <ListItemIcon>
-            <NoteIcon />
-          </ListItemIcon>
-          <ListItemText
-            primary={
-              <Grid container justify="space-between">
-                <span>{item.title}</span>
-                <span>{item.date}</span>
-              </Grid>
-            }
-            secondary={getPreviewString(
-              item.content,
-              LENGTH_OF_NOTE_CONTENT_PREVIEW
-            )}
-          />
-        </ListItem>
-      ))}
-    </List>
-  );
-};
+const NotesList = ({ notesArray, selectedIndex, setSelectedIndex }) => (
+  <List>
+    {notesArray.map((item) => (
+      <ListItem
+        button
+        selected={selectedIndex === item.id}
+        onClick={() => setSelectedIndex(item.id)}
+      >
+        <ListItemIcon>
+          <NoteIcon />
+        </ListItemIcon>
+        <ListItemText
+          primary={
+            <Grid container justify="space-between">
+              <span>{item.title}</span>
+              <span>{item.date}</span>
+            </Grid>
+          }
+          secondary={getPreviewString(
+            item.content,
+            LENGTH_OF_NOTE_CONTENT_PREVIEW
+          )}
+        />
+      </ListItem>
+    ))}
+  </List>
+);
 
 NotesList.propTypes = {
   notesArray: PropTypes.arrayOf(PropTypes.object),
